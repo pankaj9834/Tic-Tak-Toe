fcfe55e2b6045bcdc909fc06be11e68e707a53a4
Pankaj <MY_NAME@example.com>
2021-12-02 20:07:15 +0530

    Tik Tak Toe Game

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Welcome to Tik Tak Toe")
+let turn = x;
+//function to change the term
+const chngeturn = () => {
+    return turn = "X" ? "0" : "X";
+}
+
+//function to Checkwin
+const checkwin = () => {
+
+
+    }
+    // game logic
+let boxes = document.getElementsByClassName("box1", "box2", "box3", "box3", "box4", "box5", "box6", "box7", "box8", "box9");
+Array.from(boxes).forEach(element => {
+    let boxtext = document.querySelector(".boxtext");
+
+});
